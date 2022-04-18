3c45e5fda5f66ca6765be0e460a3ccfd54afe089
Gulbahor <sevaraavganova05@gmail.com>
2022-04-18 23:36:42 +0500

    admin

--- chart/core/models.py
+++ chart/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Arxiv'
+        verbose_name_plural = 'Arxivlar'
+
+    nomi = models.CharField(max_length=50)
+    mazmuni = models.TextField()
+
+    def __str__(self):
+        return self.nomi
+    
+
+
+
+
