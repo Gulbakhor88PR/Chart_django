016ece35d11d59e2f7fa9be1c1f926e956af9dd7
Gulbakhor88PR <86168250+Gulbakhor88PR@users.noreply.github.com>
2022-04-19 00:37:01 +0500

    Meta

--- chart/core/models.py
+++ chart/core/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Post(models.Model):
     class Meta:
-        verbose_name = 'Arxiv'
-        verbose_name_plural = 'Arxivlar'
+        verbose_name = 'Post'
+        verbose_name_plural = 'Postlar'
 
     nomi = models.CharField(max_length=50)
-    mazmuni = models.TextField()
+    yillar = models.IntegerField()
 
     def __str__(self):
         return self.nomi
